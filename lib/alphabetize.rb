79f6beb94db59aca1a428d1ac8575ccf216eab64
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-22 02:38:50 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,10 @@
 def alphabetize(arr)
   e_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
-  arr.sort_by do |sentence|
-    puts sentence
-    sentence.split("").map do |character|
+  arr.sort_by do |phrase|
+    #puts sentence
+    
+    phrase.split("").map do |character|
       e_alphabet.index(character)
     end
   end
